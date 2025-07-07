af853502340abf0f9d0db1886d97cbea45077296
Grant Peterson <dgrantpete@gmail.com>
2025-07-06 19:36:59 -0600

    Complete revamp of DU analyzer and fixer

---
+++ FunctionJunction.Generator/Analyzers/DiscriminatedUnionAnalyzer.cs
@@ -0,0 +1,258 @@
+﻿using FunctionJunction.Generator.Internal.Attributes;
+using FunctionJunction.Generator.Internal.Helpers;
+using FunctionJunction.Generator.Internal.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace FunctionJunction.Generator.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal class DiscriminatedUnionAnalyzer : DiagnosticAnalyzer
+{
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [..
+        DiagnosticHelper.IterateDiagnostics()
+    ];
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+        context.RegisterCompilationStartAction(compilationStartContext =>
+        {
+            if (GetCompilationContext(compilationStartContext) is not { } compilationContext)
+            {
+                return;
+            }
+
+            compilationStartContext.RegisterSymbolAction(
+                analysisContext =>
+                {
+                    if (GetUnionContext(analysisContext, compilationContext) is not { } unionContext)
+                    {
+                        return;
+                    }
+
+                    AnalyzeUnionDefinition(unionContext);
+                    AnalyzeJsonPolymorphic(unionContext);
+                    AnalyzeUnionMembers(unionContext);
+                },
+                SymbolKind.NamedType
+            );
+        });
+    }
+
+    private static UnionAttributeInfo? TryGetAttributeInfo(INamedTypeSymbol unionSymbol, CompilationContext compilation)
+    {
+        var maybeAttributeData = unionSymbol.GetAttributes()
+            .FirstOrDefault(attribute =>
+                SymbolEquals(attribute.AttributeClass, compilation.Constants.DiscriminatedUnionAttribute)
+            );
+
+        return maybeAttributeData switch
+        {
+            null => null,
+            { } attributeData => UnionAttributeInfo.FromAttributeData(attributeData)
+        };
+    }
+
+    private static void AnalyzeUnionDefinition(UnionContext union)
+    {
+        if (union.Symbol.GetObjectType() is null)
+        {
+            var notValidObjectDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.ObjectKindInvalid,
+                union.Symbol.Locations,
+                union.Symbol.Name
+            );
+
+            union.Analysis.ReportDiagnostic(notValidObjectDiagnostic);
+        }
+
+        if (union.Symbol.GetAccessibility() is null)
+        {
+            var accessibilityDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.DerivedTypeCanBeSealed,
+                union.Symbol.Locations,
+                union.Symbol.Name
+            );
+
+            union.Analysis.ReportDiagnostic(accessibilityDiagnostic);
+        }
+
+        var nonPartialSyntax = union.Symbol.DeclaringSyntaxReferences
+            .Select(syntaxReference => syntaxReference.GetSyntax())
+            .OfType<TypeDeclarationSyntax>()
+            .Where(syntax => !syntax.Modifiers.Any(SyntaxKind.PartialKeyword));
+
+        foreach (var needsPartialSyntax in nonPartialSyntax)
+        {
+            var partialMissingDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.NotMarkedPartial,
+                [needsPartialSyntax.Identifier.GetLocation()],
+                needsPartialSyntax.Identifier.Text
+            );
+
+            union.Analysis.ReportDiagnostic(partialMissingDiagnostic);
+        }
+
+        if (union.Settings.MatchOn is not MatchUnionOn.None && union.Compilation.LanguageVersion < LanguageVersion.CSharp8)
+        {
+            var languageVersionDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.SwitchExpressionsNotSupported,
+                union.Symbol.Locations,
+                union.Compilation.LanguageVersion.ToDisplayString()
+            );
+
+            union.Analysis.ReportDiagnostic(languageVersionDiagnostic);
+        }
+    }
+
+    private static void AnalyzeJsonPolymorphic(UnionContext union)
+    {
+        if (union.UserSettings.GeneratePolymorphicSerialization is not true)
+        {
+            return;
+        }
+
+        if (union.Compilation.Constants.JsonDerivedTypeAttribute is null)
+        {
+            var notFoundDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.DerivedTypeAttributeNotFound,
+                union.Symbol.Locations,
+                union.Symbol.Name
+            );
+
+            union.Analysis.ReportDiagnostic(notFoundDiagnostic);
+        }
+
+        if (union.Symbol.IsGenericType)
+        {
+            var genericDiagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.GenericsIncompatibleWithSerialization,
+                union.Symbol.Locations,
+                union.Symbol.Name
+            );
+
+            union.Analysis.ReportDiagnostic(genericDiagnostic);
+        }
+    }
+
+    private static void AnalyzeUnionMembers(UnionContext union)
+    {
+        var memberSymbols = union.Symbol.GetTypeMembers()
+            .Where(member => SymbolEquals(member.BaseType, union.Symbol))
+            .ToImmutableArray();
+
+        if (memberSymbols is [])
+        {
+            var diagnostic = DiagnosticHelper.Create(
+                DiagnosticHelper.MissingDerivedTypes,
+                union.Symbol.Locations,
+                union.Symbol.Name
+            );
+
+            union.Analysis.ReportDiagnostic(diagnostic);
+        }
+
+        foreach (var memberSymbol in memberSymbols)
+        {
+            if (memberSymbol.GetAccessibility() is null)
+            {
+                var accessibilityDiagnostic = DiagnosticHelper.Create(
+                    DiagnosticHelper.DerivedTypeAccessibilityInvalid,
+                    memberSymbol.Locations,
+                    memberSymbol.Name
+                );
+
+                union.Analysis.ReportDiagnostic(accessibilityDiagnostic);
+            }
+
+            var unsealedMembers = memberSymbol.DeclaringSyntaxReferences
+                .Select(syntaxReference => syntaxReference.GetSyntax())
+                .OfType<TypeDeclarationSyntax>()
+                .Where(syntax => !syntax.Modifiers.Any(SyntaxKind.SealedKeyword));
+
+            foreach (var unsealedMember in unsealedMembers)
+            {
+                var unsealedMemberDiagnostic = DiagnosticHelper.Create(
+                    DiagnosticHelper.DerivedTypeCanBeSealed,
+                    [unsealedMember.Identifier.GetLocation()],
+                    unsealedMember.Identifier.Text
+                );
+
+                union.Analysis.ReportDiagnostic(unsealedMemberDiagnostic);
+            }
+        }
+    }
+
+#pragma warning disable RS1012
+    private static CompilationContext? GetCompilationContext(CompilationStartAnalysisContext startAnalysisContext) => 
+        startAnalysisContext.Compilation switch
+        {
+            CSharpCompilation cSharpCompilation => new(
+                GetConstantSymbols(cSharpCompilation),
+                cSharpCompilation.LanguageVersion,
+                UnionAttributeInfo.GetDefaults(startAnalysisContext.Options.AnalyzerConfigOptionsProvider.GlobalOptions)
+            ),
+            _ => null
+        };
+#pragma warning restore RS1012
+
+    private static ConstantSymbols GetConstantSymbols(Compilation compilation)
+    {
+        var unionAttribute = compilation.GetTypeByMetadataName(DiscriminatedUnion.AttributeName)
+            ?? throw new TypeLoadException($"Could not find the unionSymbol for {DiscriminatedUnion.AttributeName}");
+
+        var jsonDerivedTypeAttribute = compilation.GetTypeByMetadataName(TypeName.JsonDerivedTypeAttribute);
+
+        return new(unionAttribute, jsonDerivedTypeAttribute);
+    }
+
+    private static UnionContext? GetUnionContext(SymbolAnalysisContext analysisContext, CompilationContext compilation)
+    {
+        var unionSymbol = (INamedTypeSymbol)analysisContext.Symbol;
+
+        if (TryGetAttributeInfo(unionSymbol, compilation) is not { } attributeInfo)
+        {
+            return null;
+        }
+
+        var userSettings = attributeInfo.Or(compilation.DefaultSettings);
+        var settings = userSettings.ToSettings();
+
+        var unionContext = new UnionContext(
+            unionSymbol,
+            settings,
+            userSettings,
+            analysisContext,
+            compilation
+        );
+
+        return unionContext;
+    }
+
+    private static bool SymbolEquals(ISymbol? first, ISymbol? second) => SymbolEqualityComparer.Default.Equals(first, second);
+
+    private readonly record struct ConstantSymbols(
+        INamedTypeSymbol DiscriminatedUnionAttribute,
+        INamedTypeSymbol? JsonDerivedTypeAttribute
+    );
+
+    private readonly record struct UnionContext(
+        INamedTypeSymbol Symbol,
+        UnionSettings Settings,
+        UnionAttributeInfo UserSettings,
+        SymbolAnalysisContext Analysis,
+        CompilationContext Compilation
+    );
+
+    private readonly record struct CompilationContext(
+        ConstantSymbols Constants,
+        LanguageVersion LanguageVersion,
+        UnionAttributeInfo DefaultSettings
+    );
+}
---
+++ FunctionJunction.Generator/Analyzers/DiscriminatedUnionFixProvider.cs
@@ -0,0 +1,260 @@
+﻿using FunctionJunction.Generator.Internal.Attributes;
+using FunctionJunction.Generator.Internal.Helpers;
+using FunctionJunction.Generator.Internal.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using System.Collections.Immutable;
+using static FunctionJunction.Generator.Internal.Helpers.DiagnosticHelper;
+
+namespace FunctionJunction.Generator.Analyzers;
+
+[ExportCodeFixProvider(LanguageNames.CSharp)]
+internal class DiscriminatedUnionFixProvider : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds => [..
+        IterateDiagnostics()
+            .Select(diagnostic => diagnostic.Id)
+    ];
+
+    public override FixAllProvider GetFixAllProvider() =>
+        WellKnownFixAllProviders.BatchFixer;
+
+    public async override Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
+            ?? throw new InvalidOperationException("Could not get syntax root from document");
+
+        foreach (var diagnostic in context.Diagnostics)
+        {
+            var diagnosticNode = root.FindNode(diagnostic.Location.SourceSpan);
+
+            RegisterCodeFix(diagnostic, diagnosticNode, context);
+        }
+    }
+
+    private static void RegisterCodeFix(
+        Diagnostic diagnostic, 
+        SyntaxNode diagnosticNode,
+        CodeFixContext fixContext
+    )
+    {
+        fixContext.CancellationToken.ThrowIfCancellationRequested();
+
+        var document = fixContext.Document;
+        
+        if (diagnostic.Id == NotMarkedPartial.Id)
+        {
+            RegisterFix(
+                "Add 'partial' modifier",
+                cancellationToken => AddDeclarationModifiers(
+                    DeclarationModifiers.Partial,
+                    diagnosticNode,
+                    document,
+                    cancellationToken
+                )
+            );
+        }
+
+        if (diagnostic.Id == DerivedTypeCanBeSealed.Id)
+        {
+            RegisterFix(
+                "Add 'sealed' modifier",
+                cancellationToken => AddDeclarationModifiers(
+                    DeclarationModifiers.Sealed,
+                    diagnosticNode,
+                    document,
+                    cancellationToken
+                )
+            );
+        }
+
+        if (diagnostic.Id == MissingDerivedTypes.Id)
+        {
+            RegisterFix(
+                "Add derived type",
+                cancellationToken => AddDerivedType(
+                    diagnosticNode,
+                    document,
+                    cancellationToken
+                )
+            );
+        }
+
+        if (diagnostic.Id == DerivedTypeAttributeNotFound.Id || diagnostic.Id == GenericsIncompatibleWithSerialization.Id)
+        {
+            RegisterFix(
+                $"Set '{nameof(DiscriminatedUnionAttribute.GeneratePolymorphicSerialization)}' to 'false'",
+                cancellationToken => UpdateAttributeArguments(
+                    new UnionAttributeInfo
+                    {
+                        GeneratePolymorphicSerialization = false
+                    },
+                    diagnosticNode,
+                    document,
+                    cancellationToken
+                )
+            );
+        }
+
+        if (diagnostic.Id == SwitchExpressionsNotSupported.Id)
+        {
+            RegisterFix(
+                $"Set '{nameof(DiscriminatedUnionAttribute.MatchOn)}' to 'None'",
+                cancellationToken => UpdateAttributeArguments(
+                    new UnionAttributeInfo
+                    {
+                        MatchOn = MatchUnionOn.None
+                    },
+                    diagnosticNode,
+                    document,
+                    cancellationToken
+                )
+            );
+        }
+
+        void RegisterFix(string title, Func<CancellationToken, Task<Document>> createChangedDocument) => 
+            fixContext.RegisterCodeFix(
+                CodeAction.Create(
+                    title,
+                    createChangedDocument,
+                    CreateEquivalenceKey(title)
+                ),
+                diagnostic
+            );
+    }
+
+    private static async Task<Document> AddDeclarationModifiers(
+        DeclarationModifiers modifiers,
+        SyntaxNode diagnosticNode,
+        Document document,
+        CancellationToken cancellationToken = default
+    )
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (diagnosticNode.FirstAncestorOrSelf<TypeDeclarationSyntax>() is not { } unionDeclaration)
+        {
+            return document;
+        }
+
+        var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+
+        var newModifiers = editor.Generator.GetModifiers(unionDeclaration) | modifiers;
+        editor.SetModifiers(unionDeclaration, newModifiers);
+
+        return editor.GetChangedDocument();
+    }
+
+    private static async Task<Document> AddDerivedType(
+        SyntaxNode diagnosticNode,
+        Document document,
+        CancellationToken cancellationToken = default
+    )
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (diagnosticNode.FirstAncestorOrSelf<TypeDeclarationSyntax>() is not { } unionDeclaration)
+        {
+            return document;
+        }
+
+        var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+
+        var unionName = unionDeclaration.Identifier.ValueText;
+
+        var unionTypeParameters = unionDeclaration.TypeParameterList
+            ?.ToString()
+            ?? string.Empty;
+
+        var derivedTypeDeclarationText = unionDeclaration switch
+        {
+            RecordDeclarationSyntax => $"public sealed record {unionName}Variant : {unionName}{unionTypeParameters};",
+            _ => $"public sealed class {unionName}Variant : {unionName}{unionTypeParameters} {{ }}"
+        };
+
+        if (SyntaxFactory.ParseMemberDeclaration(derivedTypeDeclarationText) is not { } derivedTypeDeclaration)
+        {
+            return document;
+        }
+
+        editor.AddMember(unionDeclaration, derivedTypeDeclaration.WithAdditionalAnnotations(Formatter.Annotation));
+
+        return editor.GetChangedDocument();
+    }
+
+    private static async Task<Document> UpdateAttributeArguments(
+        UnionAttributeInfo argumentUpdates,
+        SyntaxNode diagnosticNode,
+        Document document,
+        CancellationToken cancellationToken = default
+    )
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (
+            diagnosticNode.FirstAncestorOrSelf<TypeDeclarationSyntax>() is not { } unionDeclaration
+                || await document.GetSemanticModelAsync(cancellationToken) is not { } semanticModel
+                || semanticModel.Compilation.GetTypeByMetadataName(DiscriminatedUnion.AttributeName) is not { } attributeSymbol
+                || semanticModel.GetDeclaredSymbol(unionDeclaration, cancellationToken) is not { } unionSymbol
+        )
+        {
+            return document;
+        }
+
+        var attributeData = unionSymbol.GetAttributes()
+            .Where(attributeData => SymbolHelper.SymbolEquals(attributeData.AttributeClass, attributeSymbol))
+            .FirstOrDefault(attributeData =>
+                attributeData.ApplicationSyntaxReference is { SyntaxTree: var syntaxTree, Span: var span }
+                    && unionDeclaration.SyntaxTree == syntaxTree
+                    && unionDeclaration.FullSpan.Contains(span)
+            );
+
+        if (attributeData.ApplicationSyntaxReference is not { Span: var attributeSpan })
+        {
+            return document;
+        }
+
+        var originalArguments = UnionAttributeInfo.FromAttributeData(attributeData, cancellationToken);
+        var updatedArguments = argumentUpdates.Or(originalArguments);
+
+        var editor = await DocumentEditor.CreateAsync(document, cancellationToken);
+        var generator = editor.Generator;
+
+        // We are getting the syntax via the editor rather than the 'SyntaxReference.GetSyntax'
+        // since 'DocumentEditor' doesn't play nicely with references potentially outside its own syntax root
+        var attributeSyntax = editor.OriginalRoot.FindNode(attributeSpan);
+
+        var updatedAttributeSyntax = generator.Attribute(
+            generator.GetName(attributeSyntax),
+            updatedArguments.GenerateAttributeArguments(generator)
+        );
+
+        editor.ReplaceNode(attributeSyntax, updatedAttributeSyntax);
+
+        return editor.GetChangedDocument();
+    }
+
+    private static string CreateEquivalenceKey(string title) => string.Concat(
+            title.Select(char? (c) =>
+            {
+                if (c is ' ')
+                {
+                    return '_';
+                }
+
+                if (char.IsLetter(c))
+                {
+                    return char.ToUpperInvariant(c);
+                }
+
+                return null;
+            })
+            .OfType<char>()
+        );
+
+}
