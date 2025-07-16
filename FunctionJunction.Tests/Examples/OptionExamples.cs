da66b0fa59084ab9b774afcb6574bc58f0eafe9b
Grant Peterson <dgrantpete@gmail.com>
2025-07-16 10:55:48 -0600

    Add automatically generated documentation capabilities.

---
+++ FunctionJunction.Tests/Examples/OptionExamples.cs
@@ -0,0 +1,37 @@
+﻿using FunctionJunction.Extensions;
+
+namespace FunctionJunction.Tests.Examples;
+
+public class OptionExamples
+{
+    [Fact]
+    public static void Instantiate()
+    {
+        // begin-snippet: OptionInstantiation
+        // Implicitly created
+        Option<int> implicitSome = 10;
+        Option<int> implicitNone = default;
+
+        // Explicitly created
+        var explicitSome = Option.Some("Foo");
+        var explicitNone = Option.None<string>();
+
+        // From nullable value types
+        double? definitelyForSureNotNull = null;
+        var valueOption = Option.FromNullable(definitelyForSureNotNull);
+
+        // Nullable reference types can be converted implicitly
+        string? nullableReference = "Not null";
+        Option<string> referenceOption = nullableReference;
+        // end-snippet
+    }
+
+    [Fact]
+    public static void Transform()
+    {
+        // begin-snippet: OptionTransformation
+        var a = Iterator.Generate(Console.ReadLine)
+            .Select(Option.FromNullable);
+        // end-snippet
+    }
+}
